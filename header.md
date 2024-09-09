6cbb719884d838a6fe40c9eb3384aa5cdcc92681
Bruce Smith <bruce.smith@unisys.com>
2024-09-09 18:09:23 +1000

    More doc updates

--- header.md
+++ header.md
@@ -1,3 +1,16 @@
 # <div align="center">Echidna Library for Go</div>
 
 ![background image](echidna.png)
+
+### <a name="top"></a>
+### <div align="center">Table of Contents</div>
+
+* [1. Overview](#overview)
+* [2. custom](#custom)
+* [3. logger](#logger)
+* [4. set](#set)
+* [5. terminator](#terminator)
+* [6. version](#version)
+* [7. viper](#viper)
+
+# <a name="overview">1. Overview</a>
--- make_doc.sh
+++ make_doc.sh
@@ -2,3 +2,10 @@
 
 cat header.md body.md >README.md
 rm body.md
+sed -i '/^# echidna/d' README.md
+sed -i 's/^# custom/# <a name="custom">2. custom<\/a>/' README.md
+sed -i 's/^# logger/# <a name="logger">3. logger<\/a>/' README.md
+sed -i 's/^# set/# <a name="set">4. set<\/a>/' README.md
+sed -i 's/^# terminator/# <a name="terminator">5. terminator<\/a>/' README.md
+sed -i 's/^# version/# <a name="version">6. version<\/a>/' README.md
+sed -i 's/^# viper/# <a name="viper">7. viper<\/a>/' README.md
