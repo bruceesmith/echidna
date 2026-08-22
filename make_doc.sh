9899d196fd37622d339fe40d5aa5adf98bec1aa7
bruceesmith <bruceesmith@gmail.com>
2026-08-22 18:58:35 +1000

    refactor: move tools to tools/

--- make_doc.sh
+++ make_doc.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-echo '[![Go Reference][goreference_badge]][goreference_link]' > temp1
-echo '[![Go Report Card][goreportcard_badge]][goreportcard_link]' >> temp1
-echo " " >> temp1
-echo '# <div align="center">Echidna Library for Go</div>' >> temp1
-echo " " >> temp1
-echo '![background image](echidna.png)' >> temp1
-echo " " >> temp1
-echo " " > temp2
-echo '[goreference_badge]: https://pkg.go.dev/badge/github.com/bruceesmith/echidna/v3.svg' >> temp2
-echo '[goreference_link]: https://pkg.go.dev/github.com/bruceesmith/echidna' >> temp2
-echo '[goreportcard_badge]: https://goreportcard.com/badge/github.com/bruceesmith/echidna' >> temp2
-echo '[goreportcard_link]: https://goreportcard.com/report/github.com/bruceesmith/echidna' >> temp2
-go tool gomarkdoc ./... --output read
-cat temp1 read temp2 > README.md
+echo '[![Go Reference][goreference_badge]][goreference_link]' >temp1
+echo '[![Go Report Card][goreportcard_badge]][goreportcard_link]' >>temp1
+echo " " >>temp1
+echo '# <div align="center">Echidna Library for Go</div>' >>temp1
+echo " " >>temp1
+echo '![background image](echidna.png)' >>temp1
+echo " " >>temp1
+echo " " >temp2
+echo '[goreference_badge]: https://pkg.go.dev/badge/github.com/bruceesmith/echidna/v3.svg' >>temp2
+echo '[goreference_link]: https://pkg.go.dev/github.com/bruceesmith/echidna' >>temp2
+echo '[goreportcard_badge]: https://goreportcard.com/badge/github.com/bruceesmith/echidna' >>temp2
+echo '[goreportcard_link]: https://goreportcard.com/report/github.com/bruceesmith/echidna' >>temp2
+go tool -modfile=tools/go.mod gomarkdoc ./... --output read
+cat temp1 read temp2 >README.md
 rm temp1 temp2 read
